1e1c93b27dc31de9dc06fd9fb18a747254d36a6c
Jonathan Elsner <jeelsner@outlook.com>
2025-06-18 09:56:59 -0700

    Start of multigraphs

---
+++ src/optimization/graph/multigraph.py
@@ -0,0 +1,16 @@
+from typing import TypeVar, Generic, Collection, Dict
+
+from .graph import Graph, V
+
+P = TypeVar('P')
+
+class Multigraph(Graph[V], Generic[V, P]):
+    def __init__(self, vertices: Collection[V]):
+        self.vertices = set(vertices)
+        self.planes: Dict[P, Graph[V]] = dict()
+
+    def __getitem__(self, plane: P):
+        return self.planes[plane]
+
+    def __setitem__(self, plane: P, graph: Graph[V]):
+        self.planes[plane] = graph
