5b7db2e159f13749ef83e1a15920d5d9c41c37ac
Jonathan Elsner <jeelsner@outlook.com>
2025-06-22 16:31:21 -0700

    Modify IncidenceMatrix for arbitrary vertices

--- src/optimization/graph/incidence_matrix_graph.py
+++ src/optimization/graph/incidence_matrix_graph.py
@@ -1,40 +1,48 @@
-from typing import Collection, Set, Tuple
+from typing import Collection, Set, Tuple, List
 import numpy as np
 
-from .graph import Graph
+from ..set.sorted_set import SortedSet
+from .graph import Graph, V
 
-class IncidenceMatrix(Graph[int]):
-    def __init__(self, matrix: np.typing.ArrayLike):
+class IncidenceMatrix(Graph[V]):
+    def __init__(self, vertices: List[V], matrix: np.typing.ArrayLike):
+        # TODO: maybe make vertices a one-to-one mapping in the future, so that there's no possibility of duplicate vertices
+        self.vertices = vertices
         self.matrix = np.array(matrix)
 
-    def is_adjacent(self, v1: int, v2: int) -> bool:
+    def is_adjacent(self, v1: V, v2: V) -> bool:
         return np.any(self.matrix[v1] & self.matrix[v2]) # type: ignore
 
-    def neighbors_of(self, v: int) -> Set[int]:
+    def neighbors_of(self, v: V) -> Set[V]:
         # product of incidence matrix and vertex row gives number of connections to each vertex
-        neighbors = set(self.matrix @ self.matrix[v])
+        neighbors = set(self.matrix @ self.matrix[self.get_vertex_index(v)])
         neighbors.remove(v) # remove self from neighbor list (TODO: removes loops, which we would want to keep)
         return neighbors
 
-    def add_vertex(self, v: int):
+    def add_vertex(self, v: V):
         if v != self.matrix.shape[0]:
-            raise ValueError(f"Error: next vertex must be the next integer not already a vertex ({self.vertex_count=})")
+            raise ValueError(f"Error: next vertex must be the next Veger not already a vertex ({self.vertex_count=})")
 
         self.matrix = np.pad(self.matrix, ((0, 1), (0, 0)), mode='constant', constant_values='0')
 
-    def remove_vertex(self, v: int):
-        self.matrix =  np.concat((self.matrix[:v], self.matrix[v+1:]))
+    def remove_vertex(self, v: V):
+        idx = self.get_vertex_index(v)
+        self.matrix =  np.concat((self.matrix[:idx], self.matrix[idx+1:]))
 
-    def add_edge(self, edge: Tuple[int, int]):
+    def add_edge(self, edge: Tuple[V, V]):
         self.matrix = np.pad(self.matrix, ((0, 0), (0, 1)), mode='constant', constant_values='0')
-        self.matrix[edge[0], -1] = self.matrix[edge[1], -1] = 1
+        self.matrix[self.get_vertex_index(edge[0]), -1] = self.matrix[self.get_vertex_index(edge[1]), -1] = 1
 
-    def remove_edge(self, edge: Tuple[int, int]):
+    def remove_edge(self, edge: Tuple[V, V]):
         idx = self.get_edge_index(edge).pop()
         self.matrix =  np.concat((self.matrix[:, :idx], self.matrix[idx+1:]), axis=1)
         
+    def get_vertex_index(self, v: V) -> int:
+        """Get the incidence matrix row index of the vertex."""
+        return self.vertices.index(v)
     
-    def get_edge_index(self, edge: Tuple[int, int]) -> Set[int]:
+    def get_edge_index(self, edge: Tuple[V, V]) -> Set[int]:
+        """Get the incidence matrix column index of the provided edge."""
         return set(np.where(self.matrix[edge[0]] & self.matrix[edge[1]])) # type: ignore
 
     @property
@@ -46,19 +54,33 @@ class IncidenceMatrix(Graph[int]):
         return self.matrix.shape[1]
 
     @classmethod
-    def from_vertices_and_edges(cls, vertices: Collection[int], edges: Collection[Tuple[int, int]]) -> Graph[int]:
+    def from_vertices_and_edges(cls, vertices: Collection[V], edges: Collection[Tuple[V, V]]) -> Graph[V]:
         m = len(vertices)
         n = len(edges)
+        
+        vertices = list(vertices)
 
         matrix = np.zeros(shape=(m, n))
         for i, (v1, v2) in enumerate(edges):
-            matrix[v1, i] = matrix[v2, i] = 1
+            matrix[vertices.index(v1), i] = matrix[vertices.index(v2), i] = 1
 
-        return IncidenceMatrix(matrix)
+        return IncidenceMatrix(vertices, matrix)
 
     @classmethod
-    def _empty_graph(cls) -> Graph[int]:
-        return IncidenceMatrix(np.zeros(shape=(0, 0)))
+    def _empty_graph(cls) -> Graph[V]:
+        return IncidenceMatrix(list(), np.zeros(shape=(0, 0)))
 
     def to_char_string(self) -> str:
         raise NotImplementedError
+
+class IntIncidenceMatrix(IncidenceMatrix[int]):
+    def __init__(self, matrix: np.typing.ArrayLike):
+        matrix = np.array(matrix)
+        m = matrix.shape[0]
+        vertices = list(range(m))
+
+        super().__init__(vertices, matrix)
+    
+    def get_vertex_index(self, v: int) -> int:
+        # since in this case the vertex is the index itself, this is the identity
+        return v
